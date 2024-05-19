9af55a45497cfcbece7ea9c30839094d1cdaf75a
Xinyu Tan <114672390+gangsterkino@users.noreply.github.com>
2024-05-19 20:20:07 +0800

    Update kfold.py

--- Det4DFU/kfold.py
+++ Det4DFU/kfold.py
@@ -1,17 +1,16 @@
 
 import os, shutil
 from sklearn.model_selection import KFold
-# 按K折交叉验证划分数据集
+# K折交叉验证划分数据集
 def dataset_kfold(dataset_dir, save_path):
     data_list = os.listdir(dataset_dir)
-
 
     kf = KFold(n_splits=5, shuffle=True, random_state=12345)
 
     for i, (tr, val) in enumerate(kf.split(data_list), 1):
         print(len(tr), len(val))
         if os.path.exists(os.path.join(save_path, 'train{}.txt'.format(i))):
-            # 若该目录已存在，则先删除，用来清空数据
+
             print('清空原始数据中...')
             os.remove(os.path.join(save_path, 'train{}.txt'.format(i)))
             os.remove(os.path.join(save_path, 'val{}.txt'.format(i)))
@@ -31,7 +30,6 @@ def dataset_kfold(dataset_dir, save_path):
 """
 # 多类别
 if __name__ == '__main__':
-    # 膀胱数据集划分
     # 首次划分数据集或者重新划分数据集时运行
     dataset_kfold(os.path.join('media/Datasets', 'dfu/labels'),
                   os.path.join('media/Datasets', 'dfu'))
